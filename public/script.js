cd23cd5ae7ff5960e734613016efc2137b956957
SomayaB <somaya.bounouar@gmail.com>
2017-08-16 11:08:43 -0700

    Add GET all albums fetch function

--- public/script.js
+++ public/script.js
@@ -1,7 +1,18 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  getAllAlbums()
 
-  // code in here
+  function getAllAlbums(){
+    $.ajax({
+      method: 'GET',
+      contentType: 'application/json',
+      url: 'http://mutably.herokuapp.com/albums'
+    }).done(function(result){
+      console.log(result.albums)
+    }).catch(function(error){
+      console.log(error)
+    })
+  }
 
 });
